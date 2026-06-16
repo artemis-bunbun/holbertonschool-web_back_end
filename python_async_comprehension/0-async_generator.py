bf24474c5ab0fc5179b8ad409d72f1b9dbb8f06a
artemis-bunbun <starfruitworld@gmail.com>
2026-06-16 10:21:53 +0200

    fix: syntax + chmod

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,6 +7,6 @@ import random
 
 async def async_generator():
     """Async generator that yields 10 random numbers with 1 second delay between each."""
-    for i in range(10):
+    for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
